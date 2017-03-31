00fd9804287f7e45a2ab11be5cc0f37a1599abe1
Nick Kotenberg <email>
2017-03-31 02:32:46 -0600

    fixed one last issue

--- cluster-broker-client.js
+++ cluster-broker-client.js
@@ -29,13 +29,13 @@ ClusterBrokerClient.prototype.errors = {
 
 ClusterBrokerClient.prototype.breakDownURI = function (uri) {
   var parsedURI = url.parse(uri);
-  var ip = this.broker.options.clusterStateServerHost;
-  if (ip == 'localhost') { ip = '172.0.0.1'; }
-  var port = this.broker.options.clusterStateServerPort;
+  var brokerHost = this.broker.options.clusterBrokerServerHost;
+  if (brokerHost == 'localhost') { brokerHost = '172.0.0.1'; }
+  var brokerPort = this.broker.options.clusterBrokerServerPort;
   var hostname = parsedURI.host.replace(trailingPortNumberRegex, '');
   var result = {
-    hostname: ip ? `[::ffff:${ip}]` : hostname,
-    port: parsedURI.port
+    hostname: ip ? `[::ffff:${brokerHost}]` : hostname,
+    port: brokerPort || parsedURI.port
   };
   if (parsedURI.protocol == 'wss:' || parsedURI.protocol == 'https:') {
     result.secure = true;
