41433a2e8ceaa884f44230d60ee34dc5fde9d53e
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-02-02 18:28:21 +0100

    Make subscription fail safe with null channel names

--- hasher.js
+++ hasher.js
@@ -3,15 +3,15 @@ const crypto = require('crypto');
 function Hasher() {}
 
 Hasher.prototype.hashToIndex = function (key, modulo) {
+  key = key || 0;
   let ch;
   let hash = key;
-
   for (let i = 0; i < key.length; i++) {
     ch = key.charCodeAt(i);
     hash = ((hash << 5) - hash) + ch;
     hash = hash & hash;
   }
-  return Math.abs(hash || 0) % modulo;
+  return Math.abs(hash) % modulo;
 };
 
 Hasher.prototype.hashToHex = function (key, algorithm) {
